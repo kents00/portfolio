9edd787b3b2660c99664579956f3e8238165f138
Kent Edoloverio <kentedoloverio25@gmail.com>
2023-10-07 23:44:22 +0800

    resolve not displaying the data

--- src/Application/models.py
+++ src/Application/models.py
@@ -16,8 +16,8 @@ class FeaturedProject(models.Model):
     project_photo = models.ImageField(upload_to='media')
     project_name = models.CharField(max_length=255)
     description = models.TextField()
-    link = models.TextField(max_length=1000)
-    publish = models.DateTimeField(default=timezone.now().strftime('%Y-%m-%d'))
+    link = models.CharField(max_length=1000)
+    publish = models.DateField(default=timezone.now().strftime('%Y-%m-%d'))
 
     def __str__(self):
         return f"Project: {self.project_name}, Publish Date: {self.publish}"
