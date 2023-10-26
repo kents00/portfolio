a50131f9cf2e26cca45b5b7487f55916647ae688
Kent Edoloverio <kentedoloverio25@gmail.com>
2023-10-27 00:09:21 +0800

    removing contact form

--- src/Application/views.py
+++ src/Application/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import WorkExperience,FeaturedProject, Contact
+from .models import WorkExperience, FeaturedProject, Contact
 from django.core.mail import EmailMessage, get_connection
 from django.conf import settings
-from django.views.generic import FormView, TemplateView
-from .forms import ContactForm
-from django.urls import reverse_lazy
+
 
 def Menu(request):
     personal_projects = FeaturedProject.objects.all()
@@ -13,17 +11,6 @@ def Menu(request):
 
     context = {
         'personal_projects': personal_projects,
-        'work_experiences' : work_experiences,
+        'work_experiences': work_experiences,
     }
     return render(request, '.base/index.html', context)
-
-class ContactView(FormView):
-    template_name = '.base/index.html'
-    form_class = ContactForm
-    success_url = reverse_lazy('contact:success')
-
-    def form_valid(self, form):
-        # Calls the custom send method
-        form.send()
-        return super().form_valid(form)
-
